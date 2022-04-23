7dc7a16fae6ea111fc2ecda6205dbcda4ffea77f
Biswanath Tewari <biswanath@geekyants.com>
2022-04-23 20:27:47 +0530

    feat: ✨ home screen

---
+++ src/pages/Home/CategoryCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.scss'
+
+const CategoryCard = ({ text, type }) => {
+  return (
+    <div className={`category category--${type}`}>
+      <div className="category__blur">
+        <div className="category__text">{text.split(' ')[0]}</div>
+        <div className="category__description">{text}</div>
+      </div>
+    </div>
+  )
+}
+
+CategoryCard.propTypes = {
+  text: PropTypes.string,
+  img: PropTypes.any,
+  type: PropTypes.string,
+}
+
+export default CategoryCard
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import { Navbar } from '../../components'
+import CategoryCard from './CategoryCard'
+import { HeroSvg, LifeSvg } from '../../assets/svgs'
+import './styles.scss'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <main className="home__content padding-default">
+        <article className="home__hero">
+          <div className="hero__text padding-default">
+            <h1 className="h4"> experince life like never before through </h1>
+            <h1 className="h3">hooli views.</h1>
+          </div>
+          <img
+            src={HeroSvg}
+            className="hero__image img-responsive"
+            alt="boy skatting"
+          />
+        </article>
+        <article className="home__categories">
+          <CategoryCard
+            text="Life is either a daring adventure or nothing at all. "
+            img={LifeSvg}
+            type="life"
+          />
+          <CategoryCard
+            text="tech is another name for hooli"
+            img={LifeSvg}
+            type="tech"
+          />
+          <CategoryCard
+            text="Noodles are everybody's happy food"
+            img={LifeSvg}
+            type="noodles"
+          />
+        </article>
+      </main>
+    </div>
+  )
+}
+
+export { Home }
